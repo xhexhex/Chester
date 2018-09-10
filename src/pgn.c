d3eb69a53ab59f27e96b24619318c55174334cd7
Henrik Lindberg <morgulcore@gmail.com>
2018-09-10 09:47:22 +0300

    Make progress with fn san_to_rawcode()

--- src/pgn.c
+++ src/pgn.c
@@ -1,5 +1,6 @@
 #include <string.h>
 #include <stdio.h>
+#include <assert.h>
 
 #include "chester.h"
 #include "base.h"
@@ -13,19 +14,26 @@ static void x_san_to_rawcode_find_dest_sq( const Pos *p,
 Rawcode
 san_to_rawcode( const Pos *p, const char *san )
 {
-    if( !che_is_san(san) ) return 0;
-
-    char orig[2 + 1], dest[2 + 1], move[4 + 1] = {0};
+    char orig[2 + 1] = {0}, dest[2 + 1], move[4 + 1] = {0};
 
     x_san_to_rawcode_find_dest_sq(p, san, dest);
 
-    if( san[0] == 'O' )
+    if( san[0] == 'O' ) {
         strcpy( orig, sq_bit_to_sq_name( p->ppa[whites_turn(p) ?
             WHITE_KING : BLACK_KING] ) );
+    } else if( str_m_pat(san, "^[a-h][1-8]") ) {
+        Bitboard pawn = sq_name_to_sq_bit(dest);
+        while( !( ( pawn = sq_nav(pawn, whites_turn(p) ? SOUTH : NORTH) ) &
+            p->ppa[whites_turn(p) ? WHITE_PAWN : BLACK_PAWN] ) );
+        strcpy( orig, sq_bit_to_sq_name(pawn) );
+    } else if( str_m_pat(san, "^[a-h]x") ) {
+        orig[0] = san[0], orig[1] = dest[1] + ( whites_turn(p) ? -1 : 1 );
+    }
 
     move[0] = orig[0], move[1] = orig[1],
         move[2] = dest[0], move[3] = dest[1];
-    printf( "move = \"%s\"\n", move );
+    assert( str_m_pat(move, "^[a-h][1-8][a-h][1-8]$") );
+    printf("\"%s\"\n", move);
 
     return rawcode(move);
 }
@@ -40,9 +48,15 @@ static void
 x_san_to_rawcode_find_dest_sq( const Pos *p, const char *san, char *dest )
 {
     if( san[0] == 'O' ) {
-        const char *castling_rook =
-            sq_bit_to_sq_name( p->irp[strlen(san) < 5 ? 1 : 0] );
+        Bitboard castling_rook_bb = p->irp[strlen(san) < 5 ? 1 : 0];
+        castling_rook_bb <<= (whites_turn(p) ? 0 : 56);
+        const char *castling_rook = sq_bit_to_sq_name(castling_rook_bb);
         strcpy( dest, castling_rook );
-        return;
-    }
+        return; }
+
+    const char *c = &san[strlen(san) - 1];
+    while( !( *c >= '1' && *c <= '8' &&
+        *(c - 1) >= 'a' && *(c - 1) <= 'h' ) ) --c;
+
+    dest[0] = *(c - 1), dest[1] = *c, dest[2] = '\0';
 }
