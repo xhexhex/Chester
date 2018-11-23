b0745a52b9edbd283a143c38966df8e477d73cf9
Henrik Lindberg <morgulcore@gmail.com>
2018-11-23 10:56:53 +0200

    Add "external PERFT" functionality to chester_tester
    Discovered a problem in che_make_moves(). Repeated calls to the
    function consume huge amounts of memory. Going to investigate
    the apparent memory leak(s) with Valgrind.

---
+++ tests/ct/external_perft.h
@@ -0,0 +1,9 @@
+#ifndef EXTERNAL_PERFT_H
+#define EXTERNAL_PERFT_H
+
+typedef long long ll;
+
+void ex_perft( const char *root, int depth, ll exp_nc );
+
+#endif
+// end EXTERNAL_PERFT_H
--- tests/ct/main.c
+++ tests/ct/main.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <assert.h>
+#include <stdlib.h>
 
 #include "test_che_make_moves.h"
+#include "external_perft.h"
+#include "chester.h"
 
 int test_count, error_count;
 
 int
 main()
 {
+    /*
     test_CHE_MAKE_MOVES_with_pawn_promotions();
     test_CHE_MAKE_MOVES_with_short_game();
 
@@ -16,6 +20,21 @@ main()
     test_CHE_MAKE_MOVES_with_long_game_2();
     test_CHE_MAKE_MOVES_with_long_game_3();
 
+    ex_perft("8/8/8/8/8/8/1RP3q1/k6K w - - 19 83", 2, 1);
+
     if(!error_count)
         printf("All of the %i chester_tester tests succeeded\n", test_count);
+    */
+
+    // ex_perft("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 3, 8902);
+
+    printf("London calling");
+    for(int i = 1; i <= 10; i++) {
+        printf("."), fflush(stdout);
+        char *results = che_make_moves(
+            "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+            NULL);
+        free(results);
+    }
+    printf("\n");
 }
